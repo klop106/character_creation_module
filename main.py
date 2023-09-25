80af79a7de21863d1a0f23ab598850d32c448d4b
Вадим Веретельник <vveretel@yandex.ru>
2023-09-25 23:55:45 +0300

    docstrings added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
+    """Your character attack."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику '
                 f'равный {5 + randint(3, 5)}')
@@ -14,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Your character defence."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -23,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Use special ability."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f' «Выносливость {80 + 25}»')
@@ -35,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Start your char training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — '
               f'отличный боец ближнего боя.')
@@ -63,6 +68,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> None:
+    """Choose your char class."""
     approve_choice: None = None
     char_class: None = None
     while approve_choice != 'y':
@@ -86,7 +92,8 @@ def choice_char_class() -> None:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -98,4 +105,3 @@ def main() -> None:
     print(start_training(char_name, char_class))
 
 
-main()
